e5ad65e6b981467a7564bf8bdbc8d85afde4a850
alexa <alexa@DESKTOP-J22173Q>
2020-03-12 23:00:06 +0100

    comment modedeveloppeur

--- src/entrainement/ModeDeveloppeur.java
+++ src/entrainement/ModeDeveloppeur.java
@@ -2,4 +2,7 @@ package entrainement;
 
 public class ModeDeveloppeur {
 
+	// Supposition, activation uniquement si on tape une commande mais il faut
+	// Qu'elle soit invisible pour le joueur // rien trouver sur le net // à réfléchir 
+
 }
