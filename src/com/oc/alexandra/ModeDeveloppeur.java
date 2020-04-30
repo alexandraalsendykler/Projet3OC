747afc065a6fa7cf268f29041c904103dc672298
alexa <alexa@DESKTOP-J22173Q>
2020-04-30 21:15:39 +0200

    finito

--- src/com/oc/alexandra/ModeDeveloppeur.java
+++
@@ -1,8 +0,0 @@
-package entrainement;
-
-public class ModeDeveloppeur {
-
-	// Supposition, activation uniquement si on tape une commande mais il faut
-	// Qu'elle soit invisible pour le joueur // rien trouver sur le net // à réfléchir 
-
-}
